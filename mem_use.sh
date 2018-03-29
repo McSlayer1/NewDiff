cfbc7382a7a4161fff4a2e6942cfbccb22e63b80
Michael <collin.salyer@gmail.com>
2018-03-29 19:24:13 +0000

    Made some changes that allow mem_use.sh to be used more generally (still kind of experimental)

--- mem_use.sh
+++ mem_use.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 cmd=$1
+user=`whoami`
+echo "Command: $cmd"
 used1=`free | grep Mem | awk '{print $3}'`
 time1=`date +%s%6N`;
-bash -c "exec -a myProc $cmd > /dev/null 2>&1 &"
-PID=`ps -ef | grep -P 'myProc\s\w+\.\w+\s\w+.\w' | awk '{print $2}'`
+bash -c "exec -a $cmd > /dev/null 2>&1 &"
+temp=`ps -ef | grep -P "$cmd"` #| awk '{print $2}'`
+IFS=' ' read -r -a array <<< "$temp"
+PID="${array[1]}"
 echo "PID: $PID"
 mem=`awk 'BEGIN { used=0 }; /Rss/ { used += $2 } END { print used }' /proc/$PID/smaps`
 time2=`date +%s%6N`
-used2=`free | grep Mem | awk '{print $3'}`
+used2=`free | grep Mem | awk '{print $3}'`
 echo "time: $[time2 - time1]us"
 echo "Mem: $mem kB"
-pkill -f myProc
+pkill -f cmd_proc
