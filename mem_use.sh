4fe0ab0dde9c93bd43fbd1559733fbaa51ff44ca
Michael <collin.salyer@gmail.com>
2018-04-04 15:57:41 +0000

    Script now actually runs and terminates the command it is given, made another way of getting the memory of the running command, and tidied up the code structure so the "echo" commands are after everything has happened.

--- mem_use.sh
+++ mem_use.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 cmd=$1
 user=`whoami`
-echo "Command: $cmd"
+echo "Command:  $cmd"
 used1=`free | grep Mem | awk '{print $3}'`
 time1=`date +%s%6N`;
-bash -c "exec -a $cmd > /dev/null 2>&1 &"
+bash -c "exec $cmd > /dev/null 2>&1 &"
+#echo `ps -ef | grep -P "$cmd"`
 temp=`ps -ef | grep -P "$cmd"` #| awk '{print $2}'`
 IFS=' ' read -r -a array <<< "$temp"
 PID="${array[1]}"
-echo "PID: $PID"
-mem=`awk 'BEGIN { used=0 }; /Rss/ { used += $2 } END { print used }' /proc/$PID/smaps`
+mem1=`awk 'BEGIN { used=0 }; /Rss/ { used += $2 } END { print used }' /proc/$PID/smaps`
+mem2=`pmap -x $PID | grep -i total | awk '{print $4}'`
 time2=`date +%s%6N`
 used2=`free | grep Mem | awk '{print $3}'`
+echo "PID: $PID"
 echo "time: $[time2 - time1]us"
-echo "Mem: $mem kB"
-pkill -f cmd_proc
+echo "Mem: $mem1 kB"
+echo "Mem: $mem2 kB"
+#pkill -f cmd_proc
+pkill -f "$cmd"
